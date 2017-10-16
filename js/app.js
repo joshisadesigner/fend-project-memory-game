e260b6b57ea3ec13ef7083d5fc183f78d427abf8
Joshisadesigner <joshisadesigner@gmail.com>
2017-10-15 19:53:26 -0600

    Add li elments to an array variable

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*
  * Create a list that holds all of your cards
  */
-
+var cardArray = document.getElementsByClassName( 'card' );
 
 /*
  * Display the cards on the page
@@ -25,6 +25,7 @@ function shuffle(array) {
     return array;
 }
 
+shuffle( cardArray );
 
 /*
  * set up the event listener for a card. If a card is clicked:
