b12bba7f60bde6d9a9fe111e68a27f10c457e7c1
Josh <joshisadesigner@gmail.com>
2017-11-08 21:17:27 -0600

    Complete score functions

--- js/app.js
+++ js/app.js
@@ -1,11 +1,33 @@
 // Take html elements from the dom
 const cardElements = document.getElementsByClassName('card');
 
+// get moves element to store moves
+const movesElement = document.getElementById('moves');
+
 // get modal
 const modal = document.getElementById('modal');
 
-// get the restart button in modal
+// get modal move result
+const movesResult = document.getElementById('moves-result');
+
+// get modal stars result
+const starsRemain = document.getElementById('stars-remain');
+
+// get modal restart button
 const restart = document.getElementById('restart');
+
+// Take html elements from the dom
+const resetButton = document.getElementById('restartButton');
+
+// get start elements
+const stars = document.getElementById( 'stars' ).children;
+
+// Variable to store count of moves
+let moves = 0;
+
+// Variable to store stars ranking
+// initial value of 3 stars
+let ranking = 3;
 
 // Array variable to store flipped cards
 let cardsFlipped = [];
@@ -32,6 +54,14 @@ const Cards = {
     placeCards: function() {
         //hide modal for game won
         modal.style.display = "none";
+
+        // reset moves
+        movesElement.innerText = 0;
+
+        // reset stars
+        for( let i = 0; i < stars.length; i++ ){
+            stars[ i ].childNodes[ 0 ].className = 'fa fa-star';
+        }
 
         // clear array of matched cards
         match = [];
@@ -91,6 +121,9 @@ const Cards = {
 
         // Check if there are previous clicked cards
         if (cardsFlipped.length <= 1 && card.className !== 'card open show') {
+            
+            incrementMoves();
+
             // Add open show classe to card icon
             card.className += ' open show';
 
@@ -142,15 +175,58 @@ const Cards = {
     },
 };
 
-function gameWon() {
-    if (match.length === cardElements.length) {
-        modal.style.display = "block";
+function incrementMoves() {
+    moves += 1;
+    movesElement.innerText = moves;
+    reduceRanking();
+}
+// Reduce ranking function
+function reduceRanking() {
+    if ( moves === 32 ){
+        stars[ 2 ].childNodes[ 0 ].className += '-o';
+        ranking -= 1;
+        console.log( ranking );
+    }
+    
+    if ( moves === 52 ){
+        stars[ 1 ].childNodes[ 0 ].className += '-o';
+        ranking -= 1;
+        console.log( ranking );
+    }
+    
+    if ( moves === 82 ){
+        stars[ 0 ].childNodes[ 0 ].className += '-o';
+        ranking -= 1;
+        console.log( ranking );
     }
 }
 
-restart.addEventListener('click', function() {
-    Cards.placeCards();
-});
+function gameWon() {
+    // When all cards are matched and flipped
+    if (match.length === cardElements.length) {
+        // reveal the modal window
+        modal.style.display = 'block';
+        // change result text to moves varible
+        movesResult.innerText = moves;
+        // change stars text to starsRemain variable
+        starsRemain.innerText = ranking;
+    }
+}
+
+function restartTheGame( e ){
+    e.addEventListener('click', function() {
+        // flip cards to original position
+        // reset star ranking
+        // reset moves
+        Cards.placeCards();
+    });
+}
+
+// restar the game with the modal button
+restartTheGame( restart );
+
+// restar the game with deck button
+restartTheGame( resetButton );
 
 // Shuffle cards everytime the page is reload
 Cards.placeCards();
